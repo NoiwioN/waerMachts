3037a1cd2aa585208c0cdf183e39c35822658cd3
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-04-03 15:18:10 +0200

    Added Repositories

--- Backend/src/main/java/net/ictcampus/waerMachts/model/repositories/InseratRepository.java
+++ Backend/src/main/java/net/ictcampus/waerMachts/model/repositories/InseratRepository.java
@@ -1,9 +1,8 @@
 package net.ictcampus.waerMachts.model.repositories;
 
-import net.ictcampus.waerMachts.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User,Long> {
+public interface InseratRepository extends CrudRepository<Inserat, Long> {
 }
