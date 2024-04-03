11fc0e1c87e23b61b53dab01bda04bec4140f0ff
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-04-03 15:42:28 +0200

    User Model done

--- Backend/src/main/java/net/ictcampus/waerMachts/model/models/User.java
+++ Backend/src/main/java/net/ictcampus/waerMachts/model/models/User.java
@@ -1,10 +1,82 @@
 package net.ictcampus.waerMachts.model.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
 
 @Entity
 public class User {
     @Id
     private Long id;
+    @NotBlank
+    @NotNull
+    private String username;
+    @NotBlank
+    @NotNull
+    private String email;
+    @NotBlank
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String passwort;
+    @ManyToOne
+    @JoinColumn(name="id_ort")
+    private Ort ort;
+
+    @OneToMany(mappedBy = "user_id")
+    private Set<UserSkill> skills;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+
+    public Ort getOrt() {
+        return ort;
+    }
+
+    public void setOrt(Ort ort) {
+        this.ort = ort;
+    }
+
+    public Set<UserSkill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<UserSkill> skills) {
+        this.skills = skills;
+    }
 }
--- Backend/src/main/java/net/ictcampus/waerMachts/model/repositories/SkillRepository.java
+++ Backend/src/main/java/net/ictcampus/waerMachts/model/repositories/SkillRepository.java
@@ -1,5 +1,6 @@
 package net.ictcampus.waerMachts.model.repositories;
 
+import net.ictcampus.waerMachts.model.models.Skill;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
