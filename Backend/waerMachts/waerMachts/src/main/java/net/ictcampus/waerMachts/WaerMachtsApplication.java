ecd4bb36fb613c9c862f4c21431bc71be0ab2a70
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-04-03 11:13:29 +0200

    Backend Project

---
+++ Backend/waerMachts/waerMachts/src/main/java/net/ictcampus/waerMachts/WaerMachtsApplication.java
@@ -0,0 +1,13 @@
+package net.ictcampus.waerMachts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaerMachtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaerMachtsApplication.class, args);
+	}
+
+}
