d198cee3b35d6be43cc49f5d7462b8433f56164a
Linn Olsen <linn.olsen@ict-campus.net>
2024-04-04 15:23:24 +0200

    Inserate Index

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import InserateAPI from "../lib/api/inserate";
 
+export default function homePage ({inserat}) {
+    return (
+        <div>
+            <h1>{inserat.id}</h1>
+            <p>CHF  {inserat.preis}</p>
+        </div>
+    )
+}
+export async function getStaticProps(context) {
+    const id = context.params.id;
+    //gets the cats ids
+    const inserat = await InserateAPI.read(id);
+    return {
+        props: {inserat}, revalidate: 10
+    };
+}
 
-export default function homePage () {
-    return <h1>Hello</h1>
+export async function getStaticPaths() {
+    const inserate = await InserateAPI.readAll();
+    const paths = inserate.map(inserate => (
+        {
+            params: {id: inserate.id.toString()}
+        })
+    );
+    return {paths, fallback: true};
 }
