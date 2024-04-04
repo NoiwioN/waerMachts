d530018a4e74c03d6d61ccbf92f0491e04bd8d3d
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-04-04 11:28:55 +0200

    Layered MC Structure

---
+++ Backend/src/main/java/net/ictcampus/waerMachts/controller/services/UserSkillService.java
@@ -0,0 +1,10 @@
+package net.ictcampus.waerMachts.controller.services;
+
+import net.ictcampus.waerMachts.model.repositories.UserSkillRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSkillService {
+    private UserSkillRepository userSkillRepository;
+
+}
---
+++ Backend/src/main/java/net/ictcampus/waerMachts/model/repositories/UserSkillRepository.java
@@ -0,0 +1,7 @@
+package net.ictcampus.waerMachts.model.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserSkillRepository {
+}
