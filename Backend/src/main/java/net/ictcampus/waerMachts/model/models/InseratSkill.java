a9e16ab7b0ba54899e031ff3e56e40f29fc102a9
Fabian Zaugg <fabian.zaugg@ict-campus.net>
2024-04-04 16:11:38 +0200

    not working

--- Backend/src/main/java/net/ictcampus/waerMachts/model/models/InseratSkill.java
+++ Backend/src/main/java/net/ictcampus/waerMachts/model/models/InseratSkill.java
@@ -11,43 +11,44 @@ public class InseratSkill {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_inserat_skill;
 
-    @NotNull
-    @NotBlank
-    private Integer inserat_id;
-
-    @NotNull
-    @NotBlank
-    private Integer skill_id;
+//    @NotNull
+//    @NotBlank
+//    private Integer inserat_id;
+//
+//    @NotNull
+//    @NotBlank
+//    private Integer skill_id;
 
     @ManyToOne
-    @JoinColumn(name="inserat_id")
+    @JoinColumn(name = "inserat_id")
     Inserat inserat;
 
     @ManyToOne
-    @JoinColumn(name="skill_id")
+    @JoinColumn(name = "skill_id")
     Skill skill;
-
-    public Integer getId_inserat_skill() {
-        return id_inserat_skill;
-    }
-
-    public void setId_inserat_skill(Integer id_inserat_skill) {
-        this.id_inserat_skill = id_inserat_skill;
-    }
-
-    public Inserat getInserat() {
-        return inserat;
-    }
-
-    public void setInserat(Inserat inserat) {
-        this.inserat = inserat;
-    }
-
-    public Skill getSkill() {
-        return skill;
-    }
-
-    public void setSkill(Skill skill) {
-        this.skill = skill;
-    }
 }
+
+//    public Integer getId_inserat_skill() {
+//        return id_inserat_skill;
+//    }
+//
+//    public void setId_inserat_skill(Integer id_inserat_skill) {
+//        this.id_inserat_skill = id_inserat_skill;
+//    }
+//
+//    public Inserat getInserat() {
+//        return inserat;
+//    }
+//
+//    public void setInserat(Inserat inserat) {
+//        this.inserat = inserat;
+//    }
+//
+//    public Skill getSkill() {
+//        return skill;
+//    }
+//
+//    public void setSkill(Skill skill) {
+//        this.skill = skill;
+//    }
+//}
