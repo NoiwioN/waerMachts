f38edd1e92faa667a11a0b2629fec9f69e6fbc9b
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-04-04 15:41:00 +0200

    Added UserProfile Component for UserData

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab7e6e13980964ff"; }
+/******/ 		__webpack_require__.h = function() { return "27fd5c5df0d13cb6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
