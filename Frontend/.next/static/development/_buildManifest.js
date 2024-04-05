ae11ebc0e893b8f8a67d59151a0662d3572b746d
Mattias Zurbuchen <142228021+NoiwioN@users.noreply.github.com>
2024-04-05 10:13:12 +0200

    Delete Frontend/.next directory

--- Frontend/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/profil":["static\u002Fchunks\u002Fpages\u002Fprofil.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fprofil"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
