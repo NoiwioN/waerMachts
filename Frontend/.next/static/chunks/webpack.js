ad4355f2bb91ec6f3e6092e1ca6d1b9ddb47aee9
Julia Ebner <julia.ebner@ict-campus.net>
2024-04-05 10:06:29 +0200

    First Push

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eaf08a83e6d016bd"; }
+/******/ 		__webpack_require__.h = function() { return "d724a4f11101fd06"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ Frontend/lib/api/Inseratskills.js
@@ -0,0 +1,30 @@
+import { getJSON, BASE_URL, putJSON, postJSON, deleteJSON } from "."
+
+const URL = `${BASE_URL}/inseratskill`
+
+const InseratskillsAPI = {
+    readAll() {
+        return getJSON(`${URL}`)
+    },
+
+    read(id) {
+        return getJSON(`${URL}/${id}`)
+    },
+    create(inseratskill, token) {
+        const data = postJSON(`${URL}`, { body: inseratskill, token })
+        return data
+    },
+    update(inseratskill, token) {
+        const data = putJSON(`${URL}/${inseratskill.id}`, { body: inseratskill, token })
+        return data
+    },
+    delete(inseratskill, token) {
+        let data = null;
+        if (inseratskill != null) {
+            data = deleteJSON(`${URL}/${inseratskill.id}`, { token })
+        }
+        return data
+    }
+}
+
+export default InseratskillsAPI
