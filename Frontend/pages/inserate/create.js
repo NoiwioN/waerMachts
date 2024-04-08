f55d0ada3a1b6b9abef13fb7692a9b8593601368
Linn Olsen <linn.olsen@ict-campus.net>
2024-04-08 14:09:59 +0200

    A bit of styling and create inserat doesent work yet

--- Frontend/pages/inserate/create.js
+++ Frontend/pages/inserate/create.js
@@ -1,12 +1,13 @@
-// import styles from "./create.module.css"
+import styles from "./create.module.css"
 import {useState} from "react";
 import * as React from 'react';
 import {useRouter} from 'next/router';
 import InserateAPI from "../../lib/api/inserate";
 import SkillAPI from "../../lib/api/Skill";
+import {useGlobalContext} from "../../store";
 
 
-const defaultModel = {
+const emptyInserat = {
     darstellung_bild: '',
     titel: '',
     beschreibung: '',
@@ -21,10 +22,10 @@ const defaultModel = {
 
 export default function createInseratePage({skill}) {
 
+    const {session} = useGlobalContext()
     const [errors, setErrors] = useState("Form needs to be filled in")
-    const [inserat, setInserat] = useState(defaultModel)
+    const [inserat, setInserat] = useState(emptyInserat)
     const [isLoading, setIsLoading] = useState(false);
-    const [open, setOpen] = React.useState(false);
 
     const router = useRouter();
 
@@ -32,34 +33,31 @@ export default function createInseratePage({skill}) {
         const name = e.target.name
         const value = e.target.value
         inserat[name] = value;
-        setInserat(inserat)
-        validateInserat()
+        setInserat(inserat);
+        validateUser()
     }
 
-    const validateInserat = () => {
-        if (!inserat.darstellung_bild) {
-            setErrors("Password is needed")
-        }
+    const validateUser = () => {
         if (!inserat.titel) {
-            setErrors("Password is needed")
+            setErrors("Titel wird benötigt")
         }
         if (!inserat.beschreibung) {
-            setErrors("Password is needed")
+            setErrors("beschreibung wird benötigt")
+        }
+        if (!inserat.skill) {
+            setErrors("Skill wird benötigt")
         }
         if (!inserat.art_der_arbeit) {
-            setErrors("Password is needed")
-        }
-        if (!inserat.chf) {
-            setErrors("E-Mail is needed")
+            setErrors("Art der Arbeit wird benötigt")
         }
         if (!inserat.ort) {
-            setErrors("Password is needed")
+            setErrors("Ort wird benötigt")
         }
         if (!inserat.plz) {
-            setErrors("Password is needed")
+            setErrors("PLZ wird benötigt")
         }
         if (!inserat.strasse) {
-            setErrors("Username is needed")
+            setErrors("Strasse wird benötigt")
         }
     }
 
@@ -69,7 +67,8 @@ export default function createInseratePage({skill}) {
 
         try {
             inserat.erstellt_am = new Date().toString()
-            await InserateAPI.create(inserat);
+            console.log(inserat)
+            await InserateAPI.create(inserat, session.accessToken);
             await router.push("/");
         } catch (e) {
             console.error(e);
@@ -79,88 +78,114 @@ export default function createInseratePage({skill}) {
     };
 
     return (
-        <div>
-            <form>
-                <h2>Erstelle ein Inserat</h2>
-                <label htmlFor="title">Title</label>
+        <div className={styles.gridContainer}>
+            <img src={"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvia-log.com%2Fwp%2Fwp-content%2Fuploads%2F2021%2F09%2Fplatzhalter-bild-300x300-2.png&f=1&nofb=1&ipt=0488b8e06b1c7f80e879b2866ca4a54f1b48696a6c8dbec7297ce4be7d8ae377&ipo=images"} alt={"kein bild"} className={styles.bild}/>
+            <form className={styles.form}>
+                <label htmlFor="title" className={styles.label}>Titel</label>
                 <div>
                     <input
                         onChange={handleChange}
                         type="text"
-                        name="Titel"
-                        placeholder="Title"/>
+                        name="titel"
+                        placeholder="Titel"
+                        className={styles.title}
+                    />
                 </div>
-                <label htmlFor="title">Beschreibung</label>
+                <label htmlFor="beschreibung" className={styles.label}>Beschreibung</label>
                 <div>
-                        <textarea
-                            onChange={handleChange}
-                            type="text"
-                            name="beschreibung"
-                            placeholder="Beschreibung"/>
+                <textarea
+                    onChange={handleChange}
+                    name="beschreibung"
+                    placeholder="Beschreibung"
+                    className={styles.textarea}
+                />
                 </div>
-                <label htmlFor="title">Skill</label>
-                <label htmlFor="skill"></label>
-                <div>
-                    <input list="skills" name="skill" id="skill" placeholder={"skill"}/>
-                    <datalist id="skills">
-                        {skill.map((skill) => {
-                            return (
-                                <option key={skill.id}>
-                                    <>
-                                        <p>{skill.name}</p>
-                                    </>
-                                </option>
-                            )
-                        })}
-                    </datalist>
+                <div className={styles.inlineFields}>
+                    <div className={styles.inlineFields}>
+                        <div>
+                            <label htmlFor="skill" className={styles.label}>Skill</label>
+                            <input list="skills" name="skill" id="skill" placeholder="Skill"/>
+                            <datalist id="skills">
+                                {skill.map((skill) => {
+                                    return (
+                                        <option key={skill.id}>{skill.name}</option>
+                                    );
+                                })}
+                            </datalist>
+                        </div>
+                    </div>
+                    <div className={styles.inlineFields}>
+                        <div>
+                            <label htmlFor="ort" className={styles.label}>Ort</label>
+                            <input
+                                onChange={handleChange}
+                                type="text"
+                                name="ort"
+                                placeholder="Ort"
+                            />
+                        </div>
+                    </div>
                 </div>
-                <label htmlFor="title">Art der Arbeit</label>
-                <div>
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        name="art_der_arbeit"
-                        placeholder="Art der Arbeit"/>
+                <div className={styles.inlineFields}>
+                    <div className={styles.inlineFields}>
+                        <div>
+                            <label htmlFor="art_der_arbeit" className={styles.label}>Art der Arbeit</label>
+                            <input
+                                onChange={handleChange}
+                                type="text"
+                                name="art_der_arbeit"
+                                placeholder="Art der Arbeit"
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.inlineFields}>
+                        <div>
+                            <label htmlFor="plz" className={styles.label}>PLZ</label>
+                            <input
+                                onChange={handleChange}
+                                type="number"
+                                name="plz"
+                                placeholder="PLZ"
+                            />
+                        </div>
+                    </div>
                 </div>
-                <label htmlFor="title">CHF</label>
-                <div>
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        name="chf"
-                        placeholder="CHF"/>
-                </div>
-                <label htmlFor="title">Ort</label>
-                <div>
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        name="ort"
-                        placeholder="Ort"/>
-                </div>
-                <label htmlFor="title">PLZ</label>
-                <div>
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        name="plz"
-                        placeholder="PLZ"/>
-                </div>
-                <label htmlFor="title">Strasse</label>
-                <div>
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        name="strasse"
-                        placeholder="Strasse"/>
+                <div className={styles.inlineFields}>
+                    <div className={styles.inlineFields}>
+                        <div>
+                            <label htmlFor="chf" className={styles.label}>CHF</label>
+                            <input
+                                onChange={handleChange}
+                                type="number"
+                                name="chf"
+                                placeholder="CHF"
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.inlineFields}>
+                        <div>
+                            <label htmlFor="strasse" className={styles.label}>Strasse</label>
+                            <input
+                                onChange={handleChange}
+                                type="text"
+                                name="strasse"
+                                placeholder="Strasse"
+                            />
+                        </div>
+                    </div>
                 </div>
 
-                <button className={"button"} disabled={isLoading} onClick={handleSubmit}>
-                    {isLoading ? "...Loading" : "register"}
-                </button>
+                <div className={styles.buttonContainer}>
+                    <button className={`${styles.button} ${styles.erstellenButton}`} disabled={isLoading} onClick={handleSubmit}>
+                        {isLoading ? "...Loading" : "Erstellen"}
+                    </button>
+                    <button className={`${styles.button} ${styles.zuruckButton}`} disabled={isLoading}
+                            onClick={"/"}>
+                    </button>
+                </div>
             </form>
         </div>
-    )
+    );
 }
 
 export async function getStaticProps(context) {
