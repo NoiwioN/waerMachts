a07b5a69c88a717607e0ce667883183dc567ab5c
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-04-08 17:21:05 +0200

    Added Query in Backend

--- Backend/src/main/java/net/ictcampus/waerMachts/model/repositories/InseratRepository.java
+++ Backend/src/main/java/net/ictcampus/waerMachts/model/repositories/InseratRepository.java
@@ -1,6 +1,7 @@
 package net.ictcampus.waerMachts.model.repositories;
 
 import net.ictcampus.waerMachts.model.models.Inserat;
+import net.ictcampus.waerMachts.model.models.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface InseratRepository extends CrudRepository<Inserat, Integer> {
     @Query("SELECT i from Inserat i JOIN i.auftragnehmer_id u WHERE u.id_user=:userId")
     Iterable<Inserat> findInseratsByAuftragnehmer_id(@Param(value = "userId") Integer userId);
+    @Query("SELECT u from Inserat  i join  i.auftragnehmer_id u WHERE i.id_inserat=:inserat_id")
+    Iterable<User> findAuftraggeberByInseratId(@Param(value = "inserat_id")Integer inserat_id);
 }
