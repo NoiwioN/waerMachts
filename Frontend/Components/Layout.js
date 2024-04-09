09acc9e83a288c065944d23e40eaaab3baf13eea
phamthi <thi.pham@ict-campus.net>
2024-04-09 09:24:24 +0200

    added layout

---
+++ Frontend/Components/Layout.js
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Footer from "./Footer";
+
+
+export default function Layout({children}) {
+    return (
+        <>
+
+            <Header />
+            <main style={{ minHeight: '100vh', position: 'relative' }}>
+                <div>
+                    {children}
+                </div>
+                <Footer />
+            </main>
+
+        </>
+    );
+}
