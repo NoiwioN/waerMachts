3261c8e3db07a1cfe555621ffb1adf8cfd348a2d
phamthi <thi.pham@ict-campus.net>
2024-04-09 09:32:22 +0200

    deleted peter

--- Frontend/pages/_app.js
+++ Frontend/pages/_app.js
@@ -18,7 +18,7 @@ export default function App({Component, pageProps}) {
 
         <GlobalContextProvider>
             <Layout>
-                <Link href={"/users/PeterParker"}>Yo Peter </Link>
+
                 <Component {...pageProps} />
                 <ToastContainer/>
             </Layout>
