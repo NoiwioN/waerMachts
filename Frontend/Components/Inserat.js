13ad0d750b32d0fdd8a2fd92ed6309f350fa9a63
Fabian Zaugg <fabian.zaugg@ict-campus.net>
2024-04-09 14:59:55 +0200

    Home styled

--- Frontend/Components/Inserat.js
+++ Frontend/Components/Inserat.js
@@ -6,24 +6,22 @@ import styles from "./Inserat.module.css"
 // todo mit
 export default function Inserat({props}) {
 
-    const darstellungsBildStyle = {objectFit: "contain", zIndex: -1}
+    const darstellungsBildStyle = {objectFit: "cover", zIndex: -1}
 
     return (
-        <div >
-            {
-                console.log(`${props.darstellungs_bild}    ${props.id_inserat}`)
-            }
-                <img className={styles.inseratContainerImg}
-                    // src={"/cats_lake-nc.jpg"}
-                    src={props.darstellungs_bild}
-                    alt={"standard"}
-                    // placeholder={"blur"}
-                    //quality={100}
-                    // width={"100"}
-                    // height={"100"}
-                    // sizes={"50%"}
-                    fill style={darstellungsBildStyle}
-                />
+        <div className={styles.inseratContainer}>
+            <Image
+                src={props.darstellungs_bild}
+                alt={"Inserat-Bild"}
+                fill
+                style={darstellungsBildStyle}
+
+            />
+
+                {/*<img className={styles.inseratContainerImg}*/}
+                {/*    src={props.darstellungs_bild}*/}
+                {/*    alt={"standard"}*/}
+                {/*/>*/}
                 <div className={styles.inseratTextContainer}>
                     <p className={styles.bold}>Art: {props.art}</p>
                     <p>CHF: {props.preis}</p>
