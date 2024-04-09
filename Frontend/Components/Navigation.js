de073969ab76bf0d94be715b0e60f08b3c1a1fa3
Julia Ebner <julia.ebner@ict-campus.net>
2024-04-09 17:06:19 +0200

    i18n Home fertig

--- Frontend/Components/Navigation.js
+++ Frontend/Components/Navigation.js
@@ -3,33 +3,36 @@ import Link from "next/link"
 import { useGlobalContext } from "../store"
 
 import styles from "./Navigation.module.css"
-
+import React from "react";
+import {useTranslation} from "react-i18next";
+import i18n from "../i18n";
 export default function Navigation() {
 
     const { session, logout } = useGlobalContext()
     // const router = useRouter()
+    const {t} = useTranslation()
+    const handleChangeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    };
 
     return (
         <div className={styles.navigation}>
             <div>
                 <ul>
                     <li>
-                        <Link href="/">Home</Link>
+                        <Link href="/">{t("home")}</Link>
                     </li>
-                    <li>
-                        <Link href="/">User</Link>
-                    </li>
-
 
                     {/* Anzeige nur wenn eingeloggt und somit session existiert */}
-
-
                     <li>
-                        {session ? <Link href="/login" onClick={() => logout()} className="nav-link">Logout</Link> :
-                            <Link href="/login" className={`nav-link}`}>Login</Link>}
+                        {session ? <Link href="/login" onClick={() => logout()} className="nav-link">{t("logout")}</Link> :
+                            <Link href="/login" className={`nav-link}`}>{t("login")}</Link>}
                     </li>
+                    <button className={styles.translation} onClick={() => handleChangeLanguage("de")}>DE</button>
+                    <button className={styles.translation} onClick={() => handleChangeLanguage("ch")}>CH</button>
+
                     <li className={styles.user_img}>
-                        {session ? <Link href={`/profil`}><img src="/user.png"/></Link>:
+                        {session ? <Link href={`/profil`}><img src="/user.png"/></Link> :
                             <Link href={`/login`}><img src="/user.png"/></Link>}
                     </li>
                 </ul>
